fb25b14a2d0788c7fa840226b22a9e100d764853
Endy Muhardin <endy.muhardin@gmail.com>
2014-01-13 12:54:57 +0700

    implement hapus record

--- src/main/webapp/aplikasi.js
+++ src/main/webapp/aplikasi.js
@@ -7,4 +7,14 @@ HaloController = function($scope){
         $scope.daftarEmail.push(x);
         $scope.email = "";
     };
+
+    $scope.hapus = function(x){
+        var index = $scope.daftarEmail.indexOf(x);
+        alert("Anda yakin ingin menghapus "+x+" yang ada di index "+ index +" ?");
+        $scope.daftarEmail.splice(index, 1);
+    }
+
+    $scope.edit = function(x){
+        $scope.email = x;
+    }
 }
